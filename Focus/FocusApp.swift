4bf4dbfe80f6a736837cfc894cee16585a50c7b1
gustavorribeiro <gustavo.mribeiroxg@yahoo.com>
2023-05-24 12:31:48 -0300

    Initial Commit

---
+++ Focus/FocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusApp.swift
+//  Focus
+//
+//  Created by Gustavo de Moraes Ribeiro on 24/05/23.
+//
+
+import SwiftUI
+
+@main
+struct FocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
